381e6266268074f04adf7834154ac5404e07c47f
prajktyeole <prajkty@mkcl.org>
2017-03-22 17:16:04 +0530

    postcss-cssnext updated

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,26 @@
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
-var cssnext = require('cssnext');
 var precss = require('precss');
+var cssnext = require('postcss-cssnext');
 
 gulp.task('css', function() {
 
-    var processors = [autoprefixer, cssnext, precss];
+    var processors = [cssnext];
     return gulp.src('src/*.css')
         .pipe(postcss(processors))
         .pipe(gulp.dest('dist/'));
 });
+
+// gulp.task('css', function () {
+//     var plugins = [
+//         cssnext({browsers: ['last 1 version']}),
+//         opacity
+//     ];
+//     return gulp.src('./src/*.css')
+//         .pipe(postcss(plugins))
+//         .pipe(gulp.dest('./dest'));
+// });
 
 // gulp.task('rename', function() {
 //     return gulp.src('src/*.pcss')
