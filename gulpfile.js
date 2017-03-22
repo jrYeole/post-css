ebc1a6057b863c2840b68890acd4242ae6cec63c
Prajkt Yeole <prajktyeole@gmail.com>
2017-03-22 19:11:10 +0530

    updated gulp file with precss

--- gulpfile.js
+++ gulpfile.js
@@ -3,36 +3,13 @@ var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var precss = require('precss');
 var cssnext = require('postcss-cssnext');
+var rename = require("gulp-rename");
 
 gulp.task('css', function() {
 
-    var processors = [cssnext];
-    return gulp.src('src/*.css')
+    var processors = [cssnext, precss];
+    return gulp.src('src/*.pcss')
         .pipe(postcss(processors))
+        .pipe(rename("template.css"))
         .pipe(gulp.dest('dist/'));
 });
-
-// gulp.task('css', function () {
-//     var plugins = [
-//         cssnext({browsers: ['last 1 version']}),
-//         opacity
-//     ];
-//     return gulp.src('./src/*.css')
-//         .pipe(postcss(plugins))
-//         .pipe(gulp.dest('./dest'));
-// });
-
-// gulp.task('rename', function() {
-//     return gulp.src('src/*.pcss')
-//         .pipe(rename('template.min.css'))
-//         .pipe(sourcemaps.init())
-//         .pipe(sourcemaps.write('maps/'))
-//         .pipe(gulp.dest("dist/"));
-// });
-
-// gulp.task('default', ['autoprefixer', 'rename']);
-
-// // var watcher = gulp.watch('src/*.pcss', ['default']);
-// // watcher.on('change', function(event) {
-// //     console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-// // });
