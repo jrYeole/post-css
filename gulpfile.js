4ddb2a5a6bb2497d23f228926a56cb71e8ccda9b
Prajkt <prajktyeole@gmail.com>
2017-03-24 14:20:11 +0530

    updated with new changes

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,22 @@ var postcss = require('gulp-postcss');
 var precss = require('precss');
 var cssnext = require('postcss-cssnext');
 var rename = require("gulp-rename");
+var atImport = require("postcss-import");
+var cssnano = require('gulp-cssnano');
 
 gulp.task('css', function() {
-
-    var processors = [cssnext, precss];
+    var processors = [cssnext, precss, atImport, cssnano];
     return gulp.src('src/*.pcss')
         .pipe(postcss(processors))
         .pipe(rename("template.css"))
         .pipe(gulp.dest('dist/'));
 });
+
+gulp.task('minify', function() {
+    return gulp.src('dist/*.css')
+        .pipe(rename("template.min.css"))
+        .pipe(cssnano())
+        .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('default', ['css', 'minify']);
