a4a76d7f17522817bb56703282db55df4df4c584
Prajkt <prajktyeole@gmail.com>
2017-03-24 14:47:08 +0530

    structure change

--- gulpfile.js
+++ gulpfile.js
@@ -5,20 +5,26 @@ var cssnext = require('postcss-cssnext');
 var rename = require("gulp-rename");
 var atImport = require("postcss-import");
 var cssnano = require('gulp-cssnano');
+var rucksack = require('rucksack-css');
 
 gulp.task('css', function() {
-    var processors = [cssnext, precss, atImport, cssnano];
-    return gulp.src('src/*.pcss')
+    var processors = [cssnext, precss, atImport, cssnano, rucksack];
+    return gulp.src('src/pcss/*.pcss')
         .pipe(postcss(processors))
         .pipe(rename("template.css"))
-        .pipe(gulp.dest('dist/'));
+        .pipe(gulp.dest('dist/css/'));
 });
 
 gulp.task('minify', function() {
-    return gulp.src('dist/*.css')
+    return gulp.src('dist/css/*.css')
         .pipe(rename("template.min.css"))
         .pipe(cssnano())
-        .pipe(gulp.dest('dist/'));
+        .pipe(gulp.dest('dist/css/'));
 });
 
-gulp.task('default', ['css', 'minify']);
+gulp.task('copy', function() {
+    return gulp.src('src/index.html')
+        .pipe(gulp.dest('dist/'))
+});
+
+gulp.task('default', ['css', 'minify', 'copy']);
